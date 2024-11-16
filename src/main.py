823d5ab8545e9788ab58a8040fdd1ac7ce899360
João Brito <joaovitorb2345@gmail.com>
2024-11-16 10:20:43 -0300

    fix:arrumando

--- src/main.py
+++ src/main.py
@@ -6,21 +6,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def main(context):
     expected_token = os.getenv("EXPECTED_BEARER_TOKEN")
     auth_header = context.req.headers.get("Authorization")
 
     if not auth_header:
-        return context.error("Authorization header is missing")
+        context.error("Authorization header is missing")
+        return context.res.json({"error": "Authorization header is missing"})
 
     try:
         bearer_token = auth_header.split(" ")[1]
     except IndexError:
-        return context.error("Bearer token malformed")
+        context.error("Bearer token malformed")
+        return context.res.json({"error": "Bearer token malformed"})
 
     if bearer_token != expected_token:
-        return context.error("Unauthorized access attempt with invalid token")
+        context.error("Unauthorized access attempt with invalid token")
+        return context.res.json({"error": "Unauthorized"})
 
     client = (
         Client()
