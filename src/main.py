bc7cbd965c6ffa79e278b6547902d5f222a89b65
João Brito <joaovitorb2345@gmail.com>
2024-11-16 10:26:54 -0300

    feat:apenas api

--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
-from appwrite.client import Client
-from appwrite.services.users import Users
-from appwrite.exception import AppwriteException
 import os
 from dotenv import load_dotenv
 
@@ -24,29 +21,5 @@ def main(context):
         context.error("Unauthorized access attempt with invalid token")
         return context.res.json({"error": "Unauthorized"})
 
-    client = (
-        Client()
-        .set_endpoint(os.environ["APPWRITE_FUNCTION_API_ENDPOINT"])
-        .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
-        .set_key(context.req.headers["x-appwrite-key"])
-    )
-
-    users = Users(client)
-
-    try:
-        response = users.list()
-        context.log("Total users: " + str(response["total"]))
-    except AppwriteException as err:
-        context.error("Could not list users: " + repr(err))
-
     if context.req.path == "/ping":
         return context.res.text("Pong")
-
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
