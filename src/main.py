e2453c7735e140f7d36873939000e51fc3d5e944
João Brito <joaovitorb2345@gmail.com>
2024-11-23 22:23:13 -0300

    feat:seila

--- src/main.py
+++ src/main.py
@@ -68,21 +68,18 @@ def main(context):
     hub_token = query_params.get('hub.verify_token')
     expected_token = os.getenv('VERIFY_TOKEN')
 
-    return context.res.empty()
+    response = Response()
+    response_dict = {
+        "status_code": 200,
+        "body": ''
+    }
 
-    #
-    # response = Response()
-    # response_dict = {
-    #     "status_code": 200,
-    #     "body": ''
-    # }
-    #
-    # if hub_token == expected_token:
-    #     response._content = json.dumps({"hub.challenge": hub_challenge}).encode('utf-8')
-    #     response_dict['body'] = json.dumps(context)
-    # else:
-    #     user_message = 'Oq é a vida?'
-    #     response._content = json.dumps({"message": generate_chat_response(user_message)}).encode('utf-8')
-    #     response_dict['body'] = json.dumps(response.json())
-    #
-    # return response_dict
+    if hub_token == expected_token:
+        response._content = json.dumps({"hub.challenge": hub_challenge}).encode('utf-8')
+        response_dict['body'] = json.dumps(context)
+    else:
+        user_message = 'Oq é a vida?'
+        response._content = json.dumps({"message": generate_chat_response(user_message)}).encode('utf-8')
+        response_dict['body'] = json.dumps(response.json())
+
+    return response_dict
